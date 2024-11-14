1b352413f63e03311b77f84a619dbdd712ae4be6
Josemaria Ofurum <Josemariaofurum@gmail.com>
2024-11-14 06:37:54 +0100

    bell issue

--- src/assets/svg/bell.jsx
+++ src/assets/svg/bell.jsx
@@ -1,23 +1,8 @@
-<<<<<<< HEAD
-import React from 'react'
-
-const BellIcon = () => {
-  return (
-    <div>BellIcon</div>
-  )
-}
-
-export default BellIcon
-=======
 import React from "react";
 
 const BellIcon = () => {
   return (
-    <svg
-      viewBox="0 0 38 39"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg viewBox="0 0 38 39" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M28.5 12.7148C28.5 10.1953 27.4991 7.77892 25.7175 5.99733C23.9359 4.21573 21.5196 3.21484 19 3.21484C16.4804 3.21484 14.0641 4.21573 12.2825 5.99733C10.5009 7.77892 9.5 10.1953 9.5 12.7148C9.5 23.7982 4.75 26.9648 4.75 26.9648H33.25C33.25 26.9648 28.5 23.7982 28.5 12.7148Z"
         stroke="#1B1B1B"
@@ -37,4 +22,3 @@ const BellIcon = () => {
 };
 
 export default BellIcon;
->>>>>>> airxist-main
